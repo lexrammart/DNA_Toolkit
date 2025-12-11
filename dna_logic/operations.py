3b040c9ae5d1a4a4d69117edbab9349acd38fc06
lexrammart <81694980+lexrammart@users.noreply.github.com>
2025-12-11 17:18:53 -0600

    Refactor count_nucleotides add type hints

--- dna_logic/operations.py
+++ dna_logic/operations.py
@@ -1,5 +1,10 @@
 
 def count_nucleotides(dna_sequence: str) -> dict:
+    """
+    Counts the number of nucleotides in a DNA sequence.
+    :param dna_sequence: DNA sequence
+    :return: count: number of nucleotides
+    """
     dna_sequence = dna_sequence.upper()
     count = {
         "A": dna_sequence.count("A"),
@@ -13,7 +18,7 @@ def count_nucleotides(dna_sequence: str) -> dict:
 def dna_transcription (dna_sequence: str) -> str:
     """
     Converts a DNA strand into an RNA transcription.
-    :param dna_sequence:
-    :return:
+    :param dna_sequence: DNA strand
+    :return: RNA transcription
     """
     return dna_sequence.upper().replace("T", "U")
